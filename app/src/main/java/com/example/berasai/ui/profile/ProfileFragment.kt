1c3cf5a0f09df10e2b4b321d4381ba6302964ebc
Febriyanti Paramudita <febriyantiparamudita8@gmail.com>
2023-05-23 17:44:27 +0700

    add profile page detail

--- app/src/main/java/com/example/berasai/ui/profile/ProfileFragment.kt
+++ app/src/main/java/com/example/berasai/ui/profile/ProfileFragment.kt
@@ -12,9 +12,6 @@ import com.example.berasai.databinding.FragmentProfileBinding
 class ProfileFragment : Fragment() {
 
     private var _binding: FragmentProfileBinding? = null
-
-    // This property is only valid between onCreateView and
-    // onDestroyView.
     private val binding get() = _binding!!
 
     override fun onCreateView(
@@ -28,10 +25,6 @@ class ProfileFragment : Fragment() {
         _binding = FragmentProfileBinding.inflate(inflater, container, false)
         val root: View = binding.root
 
-        val textView: TextView = binding.textProfile
-        profileViewModel.text.observe(viewLifecycleOwner) {
-            textView.text = it
-        }
         return root
     }
 
